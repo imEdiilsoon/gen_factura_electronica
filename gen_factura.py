025d5d5fd79ff46a7abe9d691a798bdb6f1c14de
Edilson Sarmiento <ediilsonsarmiiento@gmail.com>
2025-04-12 21:35:22 -0500

    Mejorar la interacción del usuario en el script de generación de facturas electrónicas al agregar indicaciones en las entradas y formatear la salida de la factura.

--- gen_factura.py
+++ gen_factura.py
@@ -1,11 +1,12 @@
 import os
 import random
+from datetime import datetime
 
 opcion = -1
 
 while opcion != 2:
   print("\nElige una opción: \n\n | 1 - Crear una nueva Factura. \n | 2 - Salir del programa. \n")
-  opcion = int(input())
+  opcion = int(input("→ "))
   
   if opcion == 2:
     os.system("cls")
@@ -20,21 +21,21 @@ while opcion != 2:
     opcion_menu_factura = -1
     id_factura = random.randint(1000, 9000)
 
-    print("\n Ingrese el nombre del cliente: \n")
-    cliente = input("")
+    print("Ingrese el nombre del cliente:")
+    cliente = input("→ ")
 
     while opcion_menu_factura != 2:
 
-      print("\n Ingrese el nombre del producto: \n")
-      productos.append(input(""))
-      print("\n Ingrese el valor unitario del producto: $\n")
-      precios.append(int(input()))
-      print("\n Ingrese la cantidad del producto: \n")
-      cantidades.append(int(input()))
+      print("\n Ingrese el nombre del producto:")
+      productos.append(input("→ "))
+      print("\n Ingrese el valor unitario del producto:")
+      precios.append(int(input("$ ")))
+      print("\n Ingrese la cantidad del producto:")
+      cantidades.append(int(input("→ ")))
 
       os.system("cls")
       print("\nElige una opción: \n\n | 1 - Registrar otro producto. \n | 2 - Imprimir factura. \n")
-      opcion_menu_factura = int(input())
+      opcion_menu_factura = int(input("→ "))
 
       if(opcion_menu_factura == 2):
         os.system("cls")
@@ -51,10 +52,14 @@ while opcion != 2:
         iva = valor_neto * 0.19
         total = iva + valor_neto
 
+        fecha = datetime.now()
+        fecha_gen_fact = fecha.strftime('|  📅 Fecha: %d/%m/%Y | %H:%M:%S')
 
         print(f"\nValor Neto: $ {valor_neto}")
         print(f"IVA: $ {iva}")
         print(f"Total: $ {total}")
 
-        print(f"\n|  Cliente: {cliente}. !Gracias por elegirnos! ❤️\n")
+        print(f"\nCliente: {cliente}.")
+        print("Gracias por elegirnos! ❤️\n")
+        print(fecha_gen_fact)
         print("__________________________________________________________")
